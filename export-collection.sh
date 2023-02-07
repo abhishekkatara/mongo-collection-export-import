b8229b6ba4cd643dd9b774cd527464f07f642b23
abhishekkatara <abhikatara.sanskar91@gmail.com>
2023-02-06 23:54:05 -0600

    fix: read manual configurations

--- export-collection.sh
+++ export-collection.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
-DB=$1
-DB1=$2
-PASSWD=$3
+echo "Enter Host IP"
+read HOST
+echo "Enter Login Username"
+read USER
+echo "Enter Login Password"
+read PASSWD
+echo "Enter DB name"
+read DB1
 #DB_COLLECTIONS=$(mongo $DB --quiet --eval "db.getCollectionNames().join(' ')" --host 127.0.0.1:27017 -u the_username -p the_password --authenticationDatabase=admin)
 #custom DB collection
 DB_COLLECTIONS=(`cat collections.txt`)
 
 for collection in ${DB_COLLECTIONS[@]}}; do
     echo "Exporting $DB1/$collection ..."
-    sudo mongoexport --db=$DB1 --collection=$collection --out=$collection.json --host 127.0.0.1:27017 -u $DB -p $PASSWD --authenticationDatabase=admin
+    sudo mongoexport --db=$DB1 --collection=$collection --out=$collection.json --host $HOST:27017 -u $USER -p $PASSWD --authenticationDatabase=admin
 done
 
 
